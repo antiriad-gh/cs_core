2e3690eef27bcfde7575792a9aff84dc4c671731
antiriad-gh <49296009+antiriad-gh@users.noreply.github.com>
2024-02-09 18:57:32 -0300

    chg: span read/write

--- Antiriad.Core/src/IO/SpanWriter.cs
+++ Antiriad.Core/src/IO/SpanWriter.cs
@@ -15,22 +15,64 @@ public ref struct SpanWriter
     this.le = le;
   }
 
-  private readonly void Check()
+  private readonly void Check(int length)
   {
-    if (this.Index >= this.buffer.Length)
+    if (this.Index + length >= this.buffer.Length)
       throw new Exception("index greater than buffer length");
   }
 
   public void WriteByte(byte value)
   {
-    this.Check();
+    this.Check(1);
     this.buffer[this.Index++] = value;
   }
 
-  public void WriteUInt16(ushort value)
+  public void Write(ReadOnlySpan<byte> value)
   {
-    this.Check();
+    this.Check(value.Length);
+    value.CopyTo(this.buffer[this.Index..]);
+    this.Index += value.Length;
+  }
+
+  public void WriteUShort(ushort value)
+  {
+    this.Check(2);
     Bytes.FromUShort(value, this.buffer, this.Index, this.le);
     this.Index += 2;
+  }
+
+  public void WriteShort(short value)
+  {
+    this.Check(2);
+    Bytes.FromShort(value, this.buffer, this.Index, this.le);
+    this.Index += 2;
+  }
+
+  public void WriteUInt(uint value)
+  {
+    this.Check(4);
+    Bytes.FromUInt(value, this.buffer, this.Index, this.le);
+    this.Index += 4;
+  }
+
+  public void WriteInt(int value)
+  {
+    this.Check(4);
+    Bytes.FromInt(value, this.buffer, this.Index, this.le);
+    this.Index += 4;
+  }
+
+  public void WriteULong(ulong value)
+  {
+    this.Check(8);
+    Bytes.FromULong(value, this.buffer, this.Index, this.le);
+    this.Index += 8;
+  }
+
+  public void WriteLong(long value)
+  {
+    this.Check(8);
+    Bytes.FromLong(value, this.buffer, this.Index, this.le);
+    this.Index += 8;
   }
 }
