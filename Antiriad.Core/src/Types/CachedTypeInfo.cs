49c52af69025850b539e04efef70f7e75516dde0
antiriad-gh <antiriad@gmail.com>
2024-02-19 12:55:55 -0300

    chg: use dict for prop info

--- Antiriad.Core/src/Types/CachedTypeInfo.cs
+++ Antiriad.Core/src/Types/CachedTypeInfo.cs
@@ -7,18 +7,13 @@ namespace Antiriad.Core.Types;
 
 public class CachedTypeInfo
 {
-  //internal short LocalId;
-  //internal short RemoteId;
-  //public int RemotePropCount;
   public readonly Type Type;
   public readonly string Name;
-  public readonly PropertyMetadata[] Props;
+  public readonly Dictionary<string, PropertyMetadata> Props;
   public readonly ConstructorHandler Constructor;
 
-  public CachedTypeInfo(Type type /*, short localid, short remoteid*/)
+  public CachedTypeInfo(Type type)
   {
-    //this.LocalId = localid;
-    //this.RemoteId = remoteid;
     this.Type = type;
     this.Name = CachedTypeInfo.GetCleanName(this.Type.AssemblyQualifiedName);
 
@@ -28,14 +23,9 @@ public class CachedTypeInfo
     {
       var list = type.GetFields(BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance | BindingFlags.Public);
       metap.AddRange(list.Where(i => i.GetCustomAttributes(typeof(CompilerGeneratedAttribute), true).Length == 0).OrderBy(i => i.Name).Select(i => new PropertyMetadata(this.Type, i)));
-
-      // getter only fields has
-      // attribute: CompilerGeneratedAttribute
-      // name: <FieldName>k__BackingField
     }
     catch (Exception)
     {
-      //Trace.Exception(ex);
     }
 
     try
@@ -45,13 +35,7 @@ public class CachedTypeInfo
     }
     catch (Exception)
     {
-      //Trace.Exception(ex);
     }
-
-    /*for (var i = 0; i < metap.Count; i++)
-    {
-        metap[i].LocalId = (short)i;
-    }*/
 
     if (type.IsArray || type.IsPrimitive || type == Typer.TypeString)
     {
@@ -66,7 +50,7 @@ public class CachedTypeInfo
       this.Constructor = () => FormatterServices.GetUninitializedObject(type);
     }
 
-    this.Props = metap.ToArray();
+    this.Props = metap.ToDictionary(i => i.Name, i => i);
   }
 
   private static string GetCleanName(string? name)
@@ -91,139 +75,8 @@ public class CachedTypeInfo
     }
     catch (Exception)
     {
-      //Trace.Exception(ex);
     }
 
     return null;
-  }
-
-  private class HashPair
-  {
-    internal readonly string Alias;
-    internal int Id;
-
-    internal HashPair(string alias, int id)
-    {
-      this.Alias = alias;
-      this.Id = id;
-    }
-  }
-
-  private static readonly Dictionary<string, HashPair> HashAliases = new();
-  private static readonly object HashLock = new();
-
-  /*public static void Register(Type type)
-  {
-      lock (HashLock)
-      {
-          foreach (var c in type.GetNestedTypes())
-          {
-              var ca = (RegisterType[])c.GetCustomAttributes(typeof(RegisterType), false);
-
-              if (ca.Length > 0)
-              {
-                  TypeInfo.Register(c, string.IsNullOrEmpty(ca[0].Name) ? c.Name : ca[0].Name, ca[0].Id);
-              }
-          }
-      }
-  }
-
-  public static int Register(Type type, string alias, int id)
-  {
-      lock (HashLock)
-      {
-          var name = type.FullName;
-          HashPair pair;
-
-          if (!HashAliases.TryGetValue(name, out pair))
-          {
-              HashAliases.Add(name, pair = new HashPair(alias, id));
-
-              if (pair.Id == 0)
-              {
-                  pair.Id = NaibStream.CalculateHash(alias);
-              }
-
-              //Trace.Debug(string.Format("register class={0} alias={1} id={2}", name, alias, pair.Id));
-          }
-
-          return pair.Id;
-      }
-  }*/
-
-  /*public static int GetHash(object value)
-  {
-      return GetHash(value.GetType());
-  }
-
-  public static int GetHash(Type type)
-  {
-      return NaibStream.CalculateHash(type.FullName);
-  }*/
-
-  public static string GetTypeFromAlias(string value)
-  {
-    lock (HashLock)
-    {
-      foreach (var item in HashAliases)
-      {
-        if (item.Value.Alias.Equals(value))
-        {
-          return item.Key;
-        }
-      }
-
-      return value;
-    }
-  }
-
-  public static string GetAliasFromType(string value)
-  {
-    lock (HashLock)
-    {
-      foreach (var item in HashAliases)
-      {
-        if (item.Key.Equals(value))
-        {
-          return item.Value.Alias;
-        }
-      }
-
-      return CleanName(value);
-    }
-  }
-
-  public static string CleanName(string name)
-  {
-    var result = new char[name.Length];
-    var ignore = false;
-    var comma = false;
-    var index = 0;
-
-    for (var i = 0; i < name.Length; i++)
-    {
-      var c = name[i];
-
-      if (c == '[')
-      {
-        comma = true;
-      }
-      else if (c == ']')
-      {
-        comma = false;
-        ignore = false;
-      }
-      else if (comma && c == ',')
-      {
-        ignore = true;
-      }
-
-      if (!ignore)
-      {
-        result[index++] = c;
-      }
-    }
-
-    return new string(result, 0, index);
   }
 }
